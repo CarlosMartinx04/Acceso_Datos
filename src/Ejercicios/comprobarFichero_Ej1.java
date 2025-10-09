c2a866902bf611f569dc293dd290df47606d7253
carlosMartinx04 <carloscokchi04@gmail.com>
2025-10-09 04:40:05 +0200

    wsdf

---
+++ src/Ejercicios/comprobarFichero_Ej1.java
@@ -0,0 +1,19 @@
+package Ejercicios;
+
+import java.io.File;
+
+public class comprobarFichero_Ej1 {
+    public static void main(String[] args) {
+
+        File fichero1 = new File("VerInf.java");//Nombre del fichero
+        String rutaRelativa = new String("./Documentos/"+fichero1);
+        String rutaAbsoluta = new String("/home/Documentos/"+fichero1);
+
+        File f = new File(dir);
+        String[] archivos = f.list();
+
+        for()
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,18 @@ public class Main {
         File direc = new File("home/ejercicios");
         File fichero3 = new File(direc,"ejercicio3.txt");
 
+
+        /// //////////////////////////////////////////
+
+        String dir = ".";//llamamos al directorio actual
+        File f = new File(dir);
+        String[] archivos = f.list();
+        System.out.printf("Ficheros en el directorio actual: %d %n"+archivos.length);
+
+        for(int i = 0; i< archivos.length; i++){
+            File f2 = new File(f, archivos[i]);
+            System.out.printf("Nombre: %s , es fichero?: %b, es directorio? %b %n", archivos[i],f2.isFile(),f2.isDirectory());//booleano
+        }
+
     }
 }
