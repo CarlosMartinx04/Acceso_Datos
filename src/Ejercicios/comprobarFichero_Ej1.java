bebcb7ec22b99196a3ab5b4ae75ffa40e8f63d43
carlosMartinx04 <carloscokchi04@gmail.com>
2025-10-11 03:46:22 +0200

    Contenido del Profesor

--- src/Ejercicios/comprobarFichero_Ej1.java
+++ src/Ejercicios/comprobarFichero_Ej1.java
@@ -1,18 +1,45 @@
 package Ejercicios;
 
 import java.io.File;
+import java.io.IOException;
 
 public class comprobarFichero_Ej1 {
     public static void main(String[] args) {
+        File d = new File("NUEVODIR"); //directorio a crear
+        File f1 = new File(d, "FICHERO1.TXT");
+        File f2 = new File(d, "FICHERO2.TXT");
 
-        File fichero1 = new File("VerInf.java");//Nombre del fichero
-        String rutaRelativa = new String("./Documentos/"+fichero1);
-        String rutaAbsoluta = new String("/home/Documentos/"+fichero1);
+        d.mkdir();// Crea el directorio
 
-        File f = new File(dir);
-        String[] archivos = f.list();
+        try {
+            if (f1.createNewFile()) {
+                System.out.println("FICHERO1 creado correctamente...");
+            }
+            else{
+                System.out.println("No se ha podido crear el FICHERO1");
+            }
+            if (f2.createNewFile()) {
+                System.out.println("FICHERO2 creado correctamente...");
+            }
+            else{
+                System.out.println("No se ha podido crear el FICHERO2");
+            }
 
-        for()
+
+        } catch (IOException ioe) {ioe.printStackTrace();}
+        f1.renameTo(new File(d,"FICHERO1NUEVO"));//renombro el fichero 1
+
+        try {
+            File f3 = new File("NUEVODIR/FICHERO3.TXT");
+            f3.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if(f2.delete()){
+            System.out.println("Fichero borrado...");
+        }
+        else
+            System.out.println("No se ha podido borrar el fichero...");
 
 
     }
